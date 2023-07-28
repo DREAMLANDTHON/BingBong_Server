7fabb08b47b40d528b1736afb0aa5258639e6734
lukehongg <lukehongg@handong.ac.kr>
2023-07-28 19:58:54 +0900

    fixed

--- src/main/java/com/bingbong/consult/chatmessage/application/ChatMessageService.java
+++ src/main/java/com/bingbong/consult/chatmessage/application/ChatMessageService.java
@@ -2,12 +2,15 @@ package com.bingbong.consult.chatmessage.application;
 
 import com.bingbong.consult.chatmessage.domain.ChatMessage;
 import com.bingbong.consult.chatmessage.domain.ChatMessageRepo;
+import com.bingbong.consult.chatroom.application.ChatRoomService;
 import com.bingbong.consult.chatroom.domain.ChatRoom;
+import com.bingbong.consult.chatroom.domain.repo.ChatRoomRepo;
 import com.bingbong.consult.member.domain.Member;
 import com.bingbong.consult.member.domain.repository.MemberRepository;
 import com.bingbong.consult.stomp.MessageRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ChatMessageService {
@@ -15,12 +18,15 @@ public class ChatMessageService {
     private ChatMessageRepo chatMessageRepo;
 
     @Autowired
+    private ChatRoomRepo chatRoomRepo;
+
+    @Autowired
     private MemberRepository memberRepository;
 
+    @Transactional
     public void save(String roomToken, MessageRequest request) {
-        ChatRoom chatRoom =  chatMessageRepo.findByRoomToken(roomToken);
+        ChatRoom chatRoom =  chatRoomRepo.findByRoomToken(roomToken);
         Member member = memberRepository.findById(request.getSenderId()).get();
         chatMessageRepo.save(ChatMessage.from(request, chatRoom, member));
-
     }
 }

03c3c65ac99897cd0df69238b29b43fff5b1c9ef
홍성헌 <79990740+lukehongg@users.noreply.github.com>
2023-07-29 06:45:54 +0900

    chat on (#12)

--- src/main/java/com/bingbong/consult/stomp/MessageRequest.java
+++ src/main/java/com/bingbong/consult/stomp/MessageRequest.java
@@ -1,10 +1,12 @@
 package com.bingbong.consult.stomp;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class MessageRequest {
@@ -12,4 +14,13 @@ public class MessageRequest {
     private String roomToken;
     private String type;
     private String message;
+
+    public static MessageRequest worden(String message, String roomToken, String type){
+        return MessageRequest.builder()
+                .senderId(77L)
+                .roomToken(roomToken)
+                .type("message")
+                .message(message)
+                .build();
+    }
 }

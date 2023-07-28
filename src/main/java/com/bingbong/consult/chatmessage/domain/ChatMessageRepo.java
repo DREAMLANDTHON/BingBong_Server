5943b4314b3dfa274e687c179f6c43fe87885271
lukehongg <lukehongg@handong.ac.kr>
2023-07-28 20:01:07 +0900

    fixed

--- src/main/java/com/bingbong/consult/chatmessage/domain/ChatMessageRepo.java
+++ src/main/java/com/bingbong/consult/chatmessage/domain/ChatMessageRepo.java
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChatMessageRepo extends JpaRepository<ChatMessage, Long> {
-    ChatRoom findByRoomToken(String roomToken);
 }

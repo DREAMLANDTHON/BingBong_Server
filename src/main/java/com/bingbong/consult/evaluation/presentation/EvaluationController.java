66910d92ac65df621acc79e6b47b7516e09bd86e
jang-yujin <jyj9747@naver.com>
2023-07-28 23:37:56 +0900

    [feat]: Controller CrossOrigin 추가

--- src/main/java/com/bingbong/consult/evaluation/presentation/EvaluationController.java
+++ src/main/java/com/bingbong/consult/evaluation/presentation/EvaluationController.java
@@ -3,14 +3,13 @@ package com.bingbong.consult.evaluation.presentation;
 import com.bingbong.consult.evaluation.application.EvaluationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api")
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
+
 public class EvaluationController {
     private final EvaluationService evaluationService;
 

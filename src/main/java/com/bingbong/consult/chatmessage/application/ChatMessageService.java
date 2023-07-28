f8577e51f1a69717c38c53a4c7a91721eadc78bc
홍성헌 <79990740+lukehongg@users.noreply.github.com>
2023-07-28 20:04:10 +0900

    Lukehongg (#7)
    * STOMP
    
    * fixed
    
    * merging
    
    * stompp
    
    * fixed
    
    * fixed

--- src/main/java/com/bingbong/consult/chatmessage/application/ChatMessageService.java
+++ src/main/java/com/bingbong/consult/chatmessage/application/ChatMessageService.java
@@ -2,12 +2,22 @@ package com.bingbong.consult.chatmessage.application;
 
 import com.bingbong.consult.chatmessage.domain.ChatMessage;
 import com.bingbong.consult.chatmessage.domain.ChatMessageRepo;
+<<<<<<< HEAD
+import com.bingbong.consult.chatroom.application.ChatRoomService;
 import com.bingbong.consult.chatroom.domain.ChatRoom;
+import com.bingbong.consult.chatroom.domain.repo.ChatRoomRepo;
+=======
+import com.bingbong.consult.chatroom.domain.ChatRoom;
+>>>>>>> main
 import com.bingbong.consult.member.domain.Member;
 import com.bingbong.consult.member.domain.repository.MemberRepository;
 import com.bingbong.consult.stomp.MessageRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+<<<<<<< HEAD
+import org.springframework.transaction.annotation.Transactional;
+=======
+>>>>>>> main
 
 @Service
 public class ChatMessageService {
@@ -15,10 +25,14 @@ public class ChatMessageService {
     private ChatMessageRepo chatMessageRepo;
 
     @Autowired
+    private ChatRoomRepo chatRoomRepo;
+
+    @Autowired
     private MemberRepository memberRepository;
 
+    @Transactional
     public void save(String roomToken, MessageRequest request) {
-        ChatRoom chatRoom =  chatMessageRepo.findByRoomToken(roomToken);
+        ChatRoom chatRoom =  chatRoomRepo.findByRoomToken(roomToken);
         Member member = memberRepository.findById(request.getSenderId()).get();
         chatMessageRepo.save(ChatMessage.from(request, chatRoom, member));
 
--- src/main/java/com/bingbong/consult/chatmessage/domain/ChatMessageRepo.java
+++ src/main/java/com/bingbong/consult/chatmessage/domain/ChatMessageRepo.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChatMessageRepo extends JpaRepository<ChatMessage, Long> {
-    ChatRoom findByRoomToken(String roomToken);
+
 }
